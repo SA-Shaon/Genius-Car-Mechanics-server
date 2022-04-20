edc852bac393fe923691f5296b336b91afeb13e8
SA-Shaon <u1903047@student.cuet.ac.bd>
2022-04-21 01:37:13 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+
+// middleware 
+app.use(cors())
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.USER_ID}:${process.env.USER_PASSWORD}@cluster0.uve83.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("garage");
+        const serviceCollection = database.collection("services");
+
+
+
+        //GET API
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollection.find({})
+            const services = await cursor.toArray();
+            res.json(services);
+        })
+
+        // GET single services
+        app.get('services/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('hitting the data from mongodb', id)
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.json(service);
+        })
+
+        //POST API
+        app.post('/service', async (req, res) => {
+            const service = req.body;
+            const result = await serviceCollection.insertOne(service);
+            res.json(result);
+        })
+
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log('Listening to the port ', port);
+})
